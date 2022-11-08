773d8d0d48424ab0c29a02ca6bb14fa74636d8b4
Ben Tucker <bwtucker4@gmail.com>
2022-11-08 14:07:24 -0800

    Built initial new post container

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,16 +1,25 @@
 import React from 'react'
+import "../styles/newPost.css"
 
 const NewPost = () => {
-  return (
-    <div className='new-post-container'>
-        <div className="profile-pic"></div>
-        <div className="new-post-info">
-            <textarea name="" id="" rows="5"></textarea>
-            <input type="file" />
-            <button>Post Trek</button>
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
+
+    return (
+        <div className='new-post-container'>
+            <div className="new-post-left">
+                <div className="avatar"></div>
+            </div>
+            <div className="new-post-right">
+                <form onSubmit={handleSubmit}>
+                    <textarea name="" id="" rows="5"></textarea>
+                    <input type="file" />
+                    <button>Post Trek</button>
+                </form>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default NewPost
